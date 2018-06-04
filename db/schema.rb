45352dd5fdcf9ee4ecd13c15f5c00fdbcee70665
Vanessa Toro <vanetoro84@gmail.com>
2018-06-04 17:16:50 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180601001841) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.float "balance"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
